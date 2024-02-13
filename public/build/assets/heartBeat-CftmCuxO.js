0644394c735458e99025d79383bfb9b81a2b570b
AjRoBSeYeR <ajrob.seyer@gmail.com>
2024-02-13 11:45:12 -0600

    generate prod assets

---
+++ public/build/assets/heartBeat-CftmCuxO.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",()=>{setInterval(()=>{fetch("/heartbeat",{method:"POST",headers:{"X-CSRF-TOKEN":document.querySelector('meta[name="csrf-token"]').getAttribute("content"),"Content-Type":"application/json"},body:JSON.stringify({active_session:!0})})},1e3)});
