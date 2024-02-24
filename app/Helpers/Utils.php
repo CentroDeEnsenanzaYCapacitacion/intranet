294ca41bf31f41856ede658c9a47cd0ca36e6786
AjRoBSeYeR <ajrob.seyer@gmail.com>
2024-02-24 11:44:31 -0600

    change provider url to test qr generation

--- app/Helpers/Utils.php
+++ app/Helpers/Utils.php
@@ -21,7 +21,7 @@ class Utils
         $writer = new PngWriter();
         $result = $writer->write($qrCode);
 
-        $url = substr(public_path(),0,-6).env('PUBLIC_PATH');
+        $url = substr(public_path(),0,-19).env('PUBLIC_PATH');
 
         $result->saveToFile($url.'assets/img/qr.png');
     }
