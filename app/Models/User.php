9dbbe038bfa5e0f4ed04db6b840cf479549c9c26
AjRoBSeYeR <ajrob.seyer@gmail.com>
2025-12-06 12:30:44 -0600

    fix model cast

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,6 +37,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        'last_login' => 'datetime',
     ];
 
     public function role()
@@ -68,7 +69,7 @@ class User extends Authenticatable
     {
         return $this->hasMany(Ticket::class);
     }
-    
+
     public function ticketMessages()
     {
         return $this->hasMany(TicketMessage::class);
