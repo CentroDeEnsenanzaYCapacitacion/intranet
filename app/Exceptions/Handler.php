d29437931cb55d99ac165bef65e4121c402ad0cc
AjRoBSeYeR <ajrob.seyer@gmail.com>
2025-12-26 21:28:38 -0600

    fix views

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -18,6 +19,12 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
 
+        });
+
+        $this->renderable(function (TokenMismatchException $e, $request) {
+            return redirect()->route('login')->withErrors([
+                'session' => 'Tu sesión ha expirado. Por favor, inicia sesión nuevamente.'
+            ]);
         });
     }
 }
