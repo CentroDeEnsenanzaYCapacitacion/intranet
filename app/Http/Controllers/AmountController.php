ba541884af2f0e1f974622809fdf3d679dcba6ad
AjRoBSeYeR <ajrob.seyer@gmail.com>
2026-01-14 10:30:22 -0600

    hide amount of no active courses

--- app/Http/Controllers/AmountController.php
+++ app/Http/Controllers/AmountController.php
@@ -20,7 +20,12 @@ class AmountController extends Controller
 
     public function getAmounts()
     {
-        $amounts = Amount::with(['crew', 'course', 'receiptType'])->get();
+        $amounts = Amount::with(['crew', 'course', 'receiptType'])
+            ->whereHas('course', function ($query) {
+                $query->where('is_active', true);
+            })
+            ->orWhereNull('course_id')
+            ->get();
         $role = $usuario = Auth::user()->role_id;
         return view('admin.catalogues.amounts.show', compact('amounts','role'));
     }
